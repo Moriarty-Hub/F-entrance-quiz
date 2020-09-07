1fd78b83d7aae0c8ad34949112490246f349b859
Xiangrui Dong <samdongxiangrui@outlook.com>
2020-09-07 21:11:38 +0800

    [xdong] feat: barely finish the requirement 4: add new student

--- src/App/student/Student.js
+++ src/App/student/Student.js
@@ -3,6 +3,29 @@ import './Student.scss';
 
 class Student extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputField: ''
+    };
+  }
+
+  onSubmit = () => {
+    fetch("http://localhost:8080/students" , {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: `${this.state.inputField}`,
+    });
+  }
+
+  onChange = (event) => {
+    this.setState({
+      inputField: event.target.value
+    })
+  }
+
   render() {
     return (
       <div className="student">
@@ -14,6 +37,11 @@ class Student extends Component {
             {this.props.students.map((student) => (
               <li key={student.id} className="student-item">{student.id}. {student.name}</li>
             ))}
+            <li>
+              <form onSubmit={this.onSubmit}>
+                <input type="text" name="studentName" onChange={this.onChange}/>
+              </form>
+            </li>
           </ul>
         </div>
       </div>
