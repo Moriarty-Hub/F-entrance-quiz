5d93014dbde0a7a959a44145a9120062e56202f7
Xiangrui Dong <samdongxiangrui@outlook.com>
2020-09-07 20:01:29 +0800

    [xdong] feat: finish the requirement 1: the student list can be seen when user visit the page, which was sorted by student id

--- src/App/App.js
+++ src/App/App.js
@@ -4,11 +4,37 @@ import Team from './team/Team';
 import Student from './student/Student';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      students: []
+    }
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:8080/students")
+      .then(response =>
+        response.json()
+      )
+      .then(students=>{
+        const value = students.map((student) => {
+          return {
+            id: student.id,
+            name: student.name
+          }
+        })
+        this.setState({
+          students: value
+        })
+      })
+  }
+
   render() {
     return (
       <div>
         <Team/>
-        <Student/>
+        <Student students={this.state.students} />
       </div>
     );
   }
--- src/App/student/Student.js
+++ src/App/student/Student.js
@@ -2,11 +2,19 @@ import React, { Component } from 'react';
 import './Student.scss';
 
 class Student extends Component {
+
   render() {
     return (
       <div className="student">
         <div className="header">
           <h3>学员列表</h3>
+        </div>
+        <div className="main">
+          <ul>
+            {this.props.students.map((student) => (
+              <li key={student.id} className="student-item">{student.id}. {student.name}</li>
+            ))}
+          </ul>
         </div>
       </div>
     );
